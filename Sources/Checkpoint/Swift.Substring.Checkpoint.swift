256faa8136b4c860f53765666c9e379f4d4b9ae6
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-09-10 09:40:14 +0200

    Keep essential package source and tests

--- Sources/Checkpoint/Swift.Substring.Checkpoint.swift
+++ Sources/Checkpoint/Swift.Substring.Checkpoint.swift
@@ -1,5 +1,5 @@
 extension Swift.Substring {
-    /// Captures the input value. Equality compares cursor position for progress detection.
+
     public struct Checkpoint: Equatable {
         internal let input: Swift.Substring
         internal init(_ input: Swift.Substring) { self.input = input }
